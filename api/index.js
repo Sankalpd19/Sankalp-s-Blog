ffbc9450077030a5cfc6c12cbc09bdf3e43fa0ec
Sankalpd19 <sanklp.d19@gmail.com>
2024-04-10 20:16:40 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
  
 mongoose.connect(process.env.MONGO).then(() => {console.log("Connected to MongoDB")}).catch((err) => {console.log("Error: ", err)});
@@ -9,3 +10,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 }); 
+
+app.use('/api/user',userRoutes);
